ef856c0d337d66dc2fa4b28e3038efc8268909fb
Marc Stevenson <marc@nzfintech.co.nz>
2020-03-26 09:41:54 +1300

    Init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ tag }}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App" something="Hai"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import {
+  AZURE_TOKEN,
+} from './constants'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  computed: {
+    tag (){
+      // return process.env.NODE_ENV;
+      // return process.env.VUE_APP_SECRET_CODE;
+      // return process.env.VUE_APP_SOMETHING;
+      console.log(AZURE_TOKEN);
+      return AZURE_TOKEN;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const AZURE_URL_PLAN_LIMITS = 'https://waterdata-dev-apis.azure-api.net/planlimits/v1/'
+export const AZURE_URL_CONSENTS = 'https://waterdata-dev-apis.azure-api.net/consentsreporting/v1/'
+export const AZURE_TOKEN = process.env.VUE_APP_SUBSCRIPTION_KEY
